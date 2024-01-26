deca216a579c5b2d2ae301350ad36ecef5b2d2c5
tonny187 <tonyoanthony@gmail.com>
2024-01-25 19:27:51 -0500

    adding new components 2

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../public/logo.png'
+import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
-    <div>Aqui va el header</div>
+    <div className={styles['container']}>
+        <div className={styles['left']}>
+        <Image src={logo} alt='logo'></Image>
+        <div>
+        <h1 className={styles['InitialText']}>Portal empresas <br />Independiente</h1>
+        </div>
+        </div>
+        <div >
+            <div className={styles['right']}>
+            <Link href="/"><h1>Nosotros</h1></Link>
+            <Link href="/"><h1>Noticias</h1></Link>
+            <Link href="/"><h1>Contacto</h1></Link>
+            <Link href="/"><h1>Centro de Ayuda</h1></Link>
+            <Link href="/"><h1>Registro</h1></Link>
+            </div>
+        </div>
+    </div>
   )
 }
 
